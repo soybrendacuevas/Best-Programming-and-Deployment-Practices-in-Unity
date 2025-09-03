8cc649e815853d8089bd0a0b6ea8de50db75d2d5
Brenda Cuevas <37563484+soybrendacuevas@users.noreply.github.com>
2025-09-03 09:21:33 -0600

    Create base_structure.sh

---
+++ M01_Fundamentos/BestPractices/base_structure.sh
@@ -0,0 +1,26 @@
+/Assets
+  /Art                # Texturas, modelos, sprites
+    /Textures
+    /Models
+    /Sprites
+  /Audio
+    /SFX
+    /Music
+  /Animations
+  /Materials
+  /Prefabs
+  /Scenes
+    /Production
+    /Sandbox          # Pruebas/experimentos
+  /Scripts
+    /Core             # Utilidades, servicios, sistemas base
+    /Gameplay         # Lógica de juego por feature
+    /UI
+  /Shaders
+  /UI
+  /Addressables       # (si usas Addressables)
+  /ThirdParty         # SDKs, paquetes externos (si no usas UPM)
+  /Plugins            # Plugins nativos/managed cuando aplique*
+  /Editor             # Herramientas del editor
+  /Resources          # Solo si es imprescindible cargar por Resources.Load*
+  /StreamingAssets    # Datos sin procesar que se copian tal cual
